4a9fd2552a03c6769e4c99f76ad4ca4debd779ae
Romain Sertelon <bluepyth@gmail.com>
2012-05-31 12:23:19 +0000

    Other views

--- app/controllers/Repository.scala
+++ app/controllers/Repository.scala
@@ -5,11 +5,11 @@ import play.api.mvc._
 
 object Repository extends Controller {
 
-  def search = Action { // Search a repo by keyword. Print a list of repos
+  def search(query: String) = Action { // Search a repo by keyword. Print a list of repos
       TODO
   }
   
-  def show = Action {   // Show one repo information
+  def show(user: String, name: String) = Action {   // Show one repo information
       TODO              //  - List of users (committers & contributors separated)
                         //  - Impact of users based on last 100 commits (graph: pie, impact, other ?)
                         //  - Commits timeline base on last 100 commits
