b0d0a6786cccfc7044680cb0f2e1b89247c8a48e
Romain Sertelon <bluepyth@gmail.com>
2012-06-01 20:38:03 +0200

    Fixes compile-time errors

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,11 +6,11 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-      Ok(views.html.index())
+      Ok(views.html.application.index())
   }
   
   def about = Action {
-      Ok(views.html.about())
+      Ok(views.html.application.about())
   }
   
 }
