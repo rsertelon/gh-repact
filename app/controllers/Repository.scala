c97e16328c54f55851fe2876887c33f3cda8c369
Romain Sertelon <bluepyth@gmail.com>
2012-06-02 01:38:58 +0200

    Got the list after a search

--- app/controllers/Repository.scala
+++ app/controllers/Repository.scala
@@ -2,11 +2,31 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.ws._
+import play.api.libs.json._
 
 object Repository extends Controller {
 
   def search(query: String) = Action { // Search a repo by keyword. Print a list of repos
-      TODO
+  	Async {
+  		WS.url("http://github.com/api/v2/json/repos/search/" + query).get().map{ response =>
+  			val repos = (Json.parse(response.body) \ "repositories") match {
+  				case JsArray(elements) => {
+  					val repos = elements.map { repo =>
+  						((repo \ "username").asOpt[String], (repo \ "name").asOpt[String])
+  					}.filter{ 
+  						case (username,name) => username.isDefined && name.isDefined 
+  					}.map{
+  						case (username,name) => (username.get, name.get)
+  					}
+  					if(repos.isEmpty) None else Some(repos)
+					}
+  				case _ => None
+  			}
+  			
+      	Ok(views.html.repository.search(query)(repos))
+      }
+    }
   }
   
   def show(user: String, name: String) = Action {   // Show one repo information
