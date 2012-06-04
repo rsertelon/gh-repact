454d6aec4bbea794171d8037707f153c493c091a
Romain Sertelon <bluepyth@gmail.com>
2012-06-05 00:40:50 +0200

    Now it looks nice

--- app/models/Repo.scala
+++ app/models/Repo.scala
@@ -1,6 +1,7 @@
 package models
 
 import java.util.Date
+import org.ocpsoft.pretty.time.PrettyTime
 
 case class Repo(owner: String, name: String, url: String, description: String)
 
@@ -9,5 +10,7 @@ case class Contributor(login: String, avatar_url: String, contributions: Int, ur
 case class Commit(committer: Contributor, date: Date, message: String, sha: String) {
 	def url(repoInfo: Repo) = "https://github.com/" + repoInfo.owner + "/" + repoInfo.name + "/commit/" + sha
 	
-	val printedMessage = if(message.length > 40) message.substring(0,40) + "..." else message
+	val printedMessage = if(message.length > 32) message.substring(0,32) + "..." else message
+	
+	val prettyDate = new PrettyTime().format(date)
 }
--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,8 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "com.github.twitter" % "bootstrap" % "2.0.3",
-      "joda-time" % "joda-time" % "2.1"
+      "joda-time" % "joda-time" % "2.1",
+      "org.ocpsoft.prettytime" % "prettytime" % "1.0.8.Final"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
